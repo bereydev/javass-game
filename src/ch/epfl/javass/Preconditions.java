7588980a3cfb2473f89454e988b7d9eceab2a2d1
Jonathan <jonathan.bereyziat@gmail.com>
2019-02-20 00:03:47 +0100

    Correctif

--- src/ch/epfl/javass/Preconditions.java
+++ src/ch/epfl/javass/Preconditions.java
@@ -1,19 +1,34 @@
 package ch.epfl.javass;
 
+/**
+ * @author jonjb
+ *
+ */
 public final class Preconditions {
 
+    /**
+     * 
+     */
     private Preconditions() {
     }
     
+    /**
+     * @param b
+     */
     public static void checkArgument(boolean b) {
         if (!b) {
             throw new IllegalArgumentException();
         }
     }
     
+    /**
+     * @param index
+     * @param size
+     * @return
+     */
     public static int checkIndex(int index, int size) {
-        if (index < 0 || index <= size) {
-            throw new IllegalArgumentException();
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
         }
         else {
             return index;
