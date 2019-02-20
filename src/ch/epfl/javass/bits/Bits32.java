8697b8da177bd070a0dddc1ad13d4194554beefb
Jonathan <jonathan.bereyziat@gmail.com>
2019-02-20 13:39:17 +0100

    Optimisation de la gestion des bitstrings

--- src/ch/epfl/javass/bits/Bits32.java
+++ src/ch/epfl/javass/bits/Bits32.java
@@ -15,17 +15,29 @@ public final class Bits32 {
     }
     
     /**
+     * @param bits
+     * @return
+     */
+    private static int bitsSize(int bits) {
+        int size=0;
+        int number = bits;
+        while (number != 0) {
+            number = number >> 1; //divide by 2
+            size++;
+        }
+        return size;
+    }
+    
+    /**
      * @param start
      * @param size
      * @return
      */
     public static int mask(int start, int size) {
-        Preconditions.checkArgument(size <= 32);
-        Preconditions.checkArgument(start >= 0);
-        Preconditions.checkArgument(start <= size);
+        Preconditions.checkArgument(size <= 32 && start >= 0 && start <= size);
         int result = ~0;
         result = result << 32 - size;
-        result = result >>> 32 - size + start;
+        result = result >>> start;
         return result;
     }
     
@@ -36,10 +48,10 @@ public final class Bits32 {
      * @return
      */
     public static int extract(int bits, int start, int size) {
-        Preconditions.checkArgument(size <= 32 || start >= 0 || start <= size);
-        int result = 0;
-        result = bits << 32 - size;
-        result = bits >>> 32 - size + start;
+        Preconditions.checkArgument(size <= 32 && start >= 0 && start <= size);
+        int result = bits;
+        result = result << 32 - size + start;
+        result = result >>> start;
         return result;
     }
     
@@ -51,11 +63,9 @@ public final class Bits32 {
      * @return
      */
     public static int pack(int v1, int s1, int v2, int s2) {
-        Preconditions.checkArgument(s1 > 0 && s1 < 32 && s2 > 0 && s2 < 32 && s1 + s2 <= 32);
-        int low_bits = v1 << 32 - s1;
-        low_bits = low_bits >>> 32 - s1;
-        int high_bits = v1 >>> 32 - s2;
-        high_bits = high_bits << 32 - s2;
+        Preconditions.checkArgument(s1 > 0 && s1 < 32 && s2 > 0 && s2 < 32 && s1 + s2 <= 32 && bitsSize(v1) <= s1 && bitsSize(v2) <= s2);
+        int low_bits = v1;
+        int high_bits = v2 << 32 - s2;
         int result = low_bits | high_bits;
         return result;
     }
