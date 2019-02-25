772fcb55e837420b7f65526b6540a41359385ac5
Alex <alexandre.santangelo@epfl.ch>
2019-02-25 19:41:17 +0100

    Skeleton of PackedScore

--- src/ch/epfl/javass/jass/Jass.java
+++ src/ch/epfl/javass/jass/Jass.java
@@ -19,6 +19,11 @@ public interface Jass {
     public static final int WINNING_POINTS = 1000;
     public static final int MATCH_ADDITIONAL_POINTS = 100;
     public static final int LAST_TRICK_ADDITIONAL_POINTS = 5;
+    
+    public enum TeamId{
+        TEAM_1, TEAM_2; 
+        
+    }
 
     public enum PlayerId {
         PLAYER_1, PLAYER_2, PLAYER_3, PLAYER_4;
@@ -28,13 +33,13 @@ public interface Jass {
 
         public static final int COUNT = 4;
 
-        // /**
-        // * @return The team a player belongs to
-        // */
-        // public TeamId team() {
-        // if(this.ordinal()%2 == 0) return TeamId.TEAM_1 ;
-        // return TeamId.TEAM_2 ;
-        // }
+         /**
+         * @return The team a player belongs to
+         */
+         public TeamId team() {
+         if(this.ordinal()%2 == 0) return TeamId.TEAM_1 ;
+         return TeamId.TEAM_2 ;
+         }
 
     }
 
@@ -64,8 +69,61 @@ public interface Jass {
 
             return 1L; 
         }
+        /**
+         * @param pkScore
+         * @param t
+         * @return
+         */
         int turnTricks(long pkScore, TeamId t) {
+            return 0; 
             
+        }
+        /**
+         * @param pkScore
+         * @param t
+         * @return
+         */
+        int turnPoints(long pkScore, TeamId t) {
+            return 0; 
+        }
+        /**
+         * @param pkScore
+         * @param t
+         * @return
+         */
+        int gamePoints(long pkScore, TeamId t) {
+            return 0; 
+        }
+        /**
+         * @param pkScore
+         * @param t
+         * @return
+         */
+        int totalPoints(long pkScore, TeamId t) {
+            return 0; 
+        }
+        /**
+         * @param pkScore
+         * @param winningTeam
+         * @param trickPoints
+         * @return
+         */
+        long withAdditionalTrick(long pkScore, TeamId winningTeam, int trickPoints) {
+            return 0; 
+        }
+        /**
+         * @param pkScore
+         * @return
+         */
+        long nextTurn(long pkScore) {
+            return 0; 
+        }
+        /**
+         * @param pkScore
+         * @return
+         */
+        String toString(long pkScore) {
+            return "Hello Jon"; 
         }
 
     }
