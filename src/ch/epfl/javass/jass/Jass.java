7a4d41a621106ea3e7bf03f45da98456fc5577e8
Alex <alexandre.santangelo@epfl.ch>
2019-02-25 19:35:32 +0100

    Skeleton of PackedScore and Score classes

--- src/ch/epfl/javass/jass/Jass.java
+++ src/ch/epfl/javass/jass/Jass.java
@@ -13,32 +13,66 @@ import ch.epfl.javass.jass.Card.Color;
  *
  */
 public interface Jass {
- 
-        public static final int HAND_SIZE = 9;
-        public static final int TRICKS_PER_TURN = 9;
-        public static final int WINNING_POINTS = 1000;
-        public static final int MATCH_ADDITIONAL_POINTS = 100;
-        public static final int LAST_TRICK_ADDITIONAL_POINTS = 5;
-        
-        
-        public enum PlayerId {
-            PLAYER_1,
-            PLAYER_2,
-            PLAYER_3,
-            PLAYER_4; 
-            
-            public static final List<PlayerId> ALL = Collections
-                    .unmodifiableList(Arrays.asList(values()));
-            
-            public static final int COUNT = 4;
-            
-//            /**
-//             * @return    The team a player belongs to 
-//             */
-//            public TeamId team() {
-//                if(this.ordinal()%2 == 0) return TeamId.TEAM_1 ; 
-//                return   TeamId.TEAM_2 ; 
-//            }
+
+    public static final int HAND_SIZE = 9;
+    public static final int TRICKS_PER_TURN = 9;
+    public static final int WINNING_POINTS = 1000;
+    public static final int MATCH_ADDITIONAL_POINTS = 100;
+    public static final int LAST_TRICK_ADDITIONAL_POINTS = 5;
+
+    public enum PlayerId {
+        PLAYER_1, PLAYER_2, PLAYER_3, PLAYER_4;
+
+        public static final List<PlayerId> ALL = Collections
+                .unmodifiableList(Arrays.asList(values()));
+
+        public static final int COUNT = 4;
+
+        // /**
+        // * @return The team a player belongs to
+        // */
+        // public TeamId team() {
+        // if(this.ordinal()%2 == 0) return TeamId.TEAM_1 ;
+        // return TeamId.TEAM_2 ;
+        // }
+
+    }
+
+    public final class PackedScore {
+
+        public static final long INITIAL = 0;
+
+        /**
+         * @param pkScore
+         * @return
+         */
+        boolean isValid(long pkScore) {
+            return true;
+        }
+
+        /**
+         * @param turnTricks1
+         * @param turnPoints1
+         * @param gamePoints1
+         * @param turnTricks2
+         * @param turnPoints2
+         * @param gamePoints2
+         * @return
+         */
+        long pack(int turnTricks1, int turnPoints1, int gamePoints1,
+                int turnTricks2, int turnPoints2, int gamePoints2) {
+
+            return 1L; 
+        }
+        int turnTricks(long pkScore, TeamId t) {
             
         }
+
+    }
+
+    public final static class Score {
+
+        // public static final Score INITIAL;
+
+    }
 }
