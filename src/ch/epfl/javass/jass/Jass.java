914cb8cf237d11eb542a3080c3a0a84f80220afd
Alex <alexandre.santangelo@epfl.ch>
2019-02-25 19:21:49 +0100

    PlayerId done

--- src/ch/epfl/javass/jass/Jass.java
+++ src/ch/epfl/javass/jass/Jass.java
@@ -1,5 +1,11 @@
 package ch.epfl.javass.jass;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import ch.epfl.javass.jass.Card.Color;
+
 /**
  * Interface that store the constants values concerning the Jass game
  * 
@@ -13,4 +19,23 @@ public interface Jass {
         public static final int WINNING_POINTS = 1000;
         public static final int MATCH_ADDITIONAL_POINTS = 100;
         public static final int LAST_TRICK_ADDITIONAL_POINTS = 5;
+        
+        
+        public enum PlayerId {
+            PLAYER_1,
+            PLAYER_2,
+            PLAYER_3,
+            PLAYER_4; 
+            
+            public static final List<PlayerId> ALL = Collections
+                    .unmodifiableList(Arrays.asList(values()));
+            
+            public static final int COUNT = 4;
+            
+//            public TeamId team() {
+//                if(this.ordinal()%2 == 0) return TeamId.TEAM_1 ; 
+//                return   TeamId.TEAM_2 ; 
+//            }
+            
+        }
 }
