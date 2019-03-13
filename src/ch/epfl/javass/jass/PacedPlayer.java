147c43516048d137344d90c7351fcfe8044e6003
Alex <alexandre.santangelo@epfl.ch>
2019-03-13 20:24:54 +0100

    PacedPlayer done

---
+++ src/ch/epfl/javass/jass/PacedPlayer.java
@@ -0,0 +1,98 @@
+/*
+ *	Author : Alexandre Santangelo 
+ *	Date   : Mar 13, 2019	
+*/
+
+/**
+ * 
+ */
+package ch.epfl.javass.jass;
+
+import java.util.Map;
+
+import ch.epfl.javass.jass.Card.Color;
+
+public final class PacedPlayer implements Player {
+    
+    private Player underlyingPlayer; 
+    private double minTime; 
+    
+    PacedPlayer(Player underlyingPlayer, double minTime){
+        this.underlyingPlayer = underlyingPlayer; 
+        this.minTime = minTime; 
+    }
+
+    /* (non-Javadoc)
+     * @see ch.epfl.javass.jass.Player#cardToPlay(ch.epfl.javass.jass.TurnState, ch.epfl.javass.jass.CardSet)
+     */
+    @Override
+    public Card cardToPlay(TurnState state, CardSet hand) {
+        double startTime = System.currentTimeMillis(); 
+        Card card = underlyingPlayer.cardToPlay(state, hand); 
+        double currentTime = System.currentTimeMillis(); 
+        if(currentTime - startTime >= minTime) {
+            return card; 
+        }
+        try {
+            Thread.sleep((int)(minTime + startTime - currentTime));
+        }
+        catch(InterruptedException e) { /* do nothing */ }
+
+        return card;
+    }
+
+    /* (non-Javadoc)
+     * @see ch.epfl.javass.jass.Player#setPlayers(ch.epfl.javass.jass.PlayerId, java.util.Map)
+     */
+    @Override
+    public void setPlayers(PlayerId ownId, Map<PlayerId, String> playerNames) {
+        underlyingPlayer.setPlayers(ownId, playerNames);
+
+    }
+
+    /* (non-Javadoc)
+     * @see ch.epfl.javass.jass.Player#updateHand(ch.epfl.javass.jass.CardSet)
+     */
+    @Override
+    public void updateHand(CardSet newHand) {
+        underlyingPlayer.updateHand(newHand);
+
+    }
+
+    /* (non-Javadoc)
+     * @see ch.epfl.javass.jass.Player#setTrump(ch.epfl.javass.jass.Card.Color)
+     */
+    @Override
+    public void setTrump(Color trump) {
+        underlyingPlayer.setTrump(trump);
+
+    }
+
+    /* (non-Javadoc)
+     * @see ch.epfl.javass.jass.Player#updateTrick(ch.epfl.javass.jass.Trick)
+     */
+    @Override
+    public void updateTrick(Trick newTrick) {
+        underlyingPlayer.updateTrick(newTrick);
+
+    }
+
+    /* (non-Javadoc)
+     * @see ch.epfl.javass.jass.Player#updateScore(ch.epfl.javass.jass.Score)
+     */
+    @Override
+    public void updateScore(Score score) {
+        underlyingPlayer.updateScore(score);
+
+    }
+
+    /* (non-Javadoc)
+     * @see ch.epfl.javass.jass.Player#setWinningTeam(ch.epfl.javass.jass.TeamId)
+     */
+    @Override
+    public void setWinningTeam(TeamId winningTeam) {
+        underlyingPlayer.setWinningTeam(winningTeam);
+
+    }
+
+}
