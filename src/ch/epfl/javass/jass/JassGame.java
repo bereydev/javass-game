93e3704b90660757865b41da54ec6a567bbbe3a0
Alex <alexandre.santangelo@epfl.ch>
2019-03-18 15:43:20 +0100

    JassGame changes

--- src/ch/epfl/javass/jass/JassGame.java
+++ src/ch/epfl/javass/jass/JassGame.java
@@ -9,11 +9,13 @@
 package ch.epfl.javass.jass;
 
 import java.util.Map;
+
 import java.util.Map.Entry;
 import java.util.Random;
 
 import ch.epfl.javass.jass.Card.Color;
 import ch.epfl.javass.jass.Card.Rank;
+import ch.epfl.javass.jass.Trick;
 
 import java.util.Collections;
 import java.util.EnumMap;
@@ -25,10 +27,11 @@ public final class JassGame {
     private long rngSeed;
     private Map<PlayerId, Player> players;
     private Map<PlayerId, String> playerNames;
+    private List<Card> cards = new LinkedList();
     private Map<PlayerId, CardSet> hands;
     private Random shuffleRng;
     private Random trumpRng;
-    private List<Card> cards = new LinkedList();
+
     private TurnState turnState;
     private static final int CARDS_PER_HAND = 9;
 
@@ -48,10 +51,6 @@ public final class JassGame {
                 cards.add(Card.ofPacked(j + (i << 4)));
             }
         }
-        deal();
-        turnState = TurnState.initial(Color.values()[trumpRng.nextInt(4)],
-                Score.INITIAL, firstPlayer());
-
     }
 
     /**
@@ -68,16 +67,37 @@ public final class JassGame {
      * Advances to the end of the next trick, doing everything.
      */
     void advanceToEndOfNextTrick() {
+        if (isGameOver()) {
+            /* does nothing */ }
+
+        else {
+            // The trick has not started.
+            if (turnState.trick().index() == 0) {
+                deal();
+                turnState = TurnState.initial(
+                        Color.values()[trumpRng.nextInt(4)], Score.INITIAL,
+                        firstPlayer());
+            }
+            do {
+                play();
+                turnState.withTrickCollected();
+                
+            } while (!turnState.trick().isLast());
+
+            turnState.score().withAdditionalTrick(
+                    turnState.trick().winningPlayer().team(),
+                    turnState.trick().points());
+        }
 
     }
 
     private void deal() {
         Collections.shuffle(cards, shuffleRng);
         for (PlayerId p : hands.keySet()) {
-            CardSet hand = CardSet.EMPTY; 
+            CardSet hand = CardSet.EMPTY;
             for (int i = CARDS_PER_HAND * p.ordinal(); i < CARDS_PER_HAND
                     * (p.ordinal() + 1); i++) {
-                hand.add(cards.get(i)); 
+                hand.add(cards.get(i));
                 // System.out.println("added card "+
                 // PackedCard.toString(cards.get(i)));
             }
@@ -87,18 +107,25 @@ public final class JassGame {
     }
 
     private PlayerId firstPlayer() {
+
         for (PlayerId p : hands.keySet()) {
             if (hands.get(p).contains(Card.of(Color.DIAMOND, Rank.SEVEN)))
                 return p;
-
         }
-        return null;
+        // This shouldn't happen
+        return PlayerId.PLAYER_1;
     }
 
     private void play() {
         for (PlayerId p : players.keySet()) {
-            players.get(p).updateHand(hands.get(p));
-            players.get(p).updateTrick(turnState.trick().withAddedCard(players.get(p).cardToPlay(turnState, hands.get(p))));
+            Card cardToPlay = players.get(p).cardToPlay(turnState,
+                    hands.get(p));
+
+            players.get(p)
+                    .updateTrick(turnState.trick().withAddedCard(cardToPlay));
+            players.get(p).updateHand(hands.get(p).remove(cardToPlay));
+            players.get(p).updateScore(turnState.score());
+            turnState.withNewCardPlayed(cardToPlay);
         }
 
     }
