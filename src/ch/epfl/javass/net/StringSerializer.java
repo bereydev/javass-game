36664718377fa078b09b9732979fe60b77007f2b
Alex <alexandre.santangelo@epfl.ch>
2019-04-15 13:18:01 +0200

    etape8 start

---
+++ src/ch/epfl/javass/net/StringSerializer.java
@@ -0,0 +1,17 @@
+/*
+ *	Author : Alexandre Santangelo 
+ *	Date   : Apr 15, 2019	
+*/
+
+/**
+ * 
+ */
+package ch.epfl.javass.net;
+
+/**
+ * @author astra
+ *
+ */
+public class StringSerializer {
+
+}
