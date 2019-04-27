99e81da4a8949697ed1cd5f92f0f27b5ed73f36c
Alex <alexandre.santangelo@epfl.ch>
2019-04-27 13:37:41 +0200

    ScoreBean done

--- src/ch/epfl/javass/gui/ScoreBean.java
+++ src/ch/epfl/javass/gui/ScoreBean.java
@@ -2,6 +2,9 @@ package ch.epfl.javass.gui;
 
 import ch.epfl.javass.jass.TeamId;
 import javafx.beans.property.ReadOnlyIntegerProperty;
+import javafx.beans.property.ReadOnlyObjectProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
 
 public final class ScoreBean {
     private int turnPoints1, turnPoints2;
@@ -10,14 +13,51 @@ public final class ScoreBean {
     private TeamId winningTeam;
 
     public ReadOnlyIntegerProperty turnPointsProperty(TeamId team) {
+        if(team.equals(TeamId.TEAM_1))
+            return new SimpleIntegerProperty(turnPoints1); 
+        return  new SimpleIntegerProperty(turnPoints2); 
     }
+    
+    public ReadOnlyIntegerProperty gamePointsProperty(TeamId team) {
+        if(team.equals(TeamId.TEAM_1))
+            return new SimpleIntegerProperty(gamePoints1); 
+        return  new SimpleIntegerProperty(gamePoints2); 
+    }
+    
+    public ReadOnlyIntegerProperty totalPointsProperty(TeamId team) {
+        if(team.equals(TeamId.TEAM_1))
+            return new SimpleIntegerProperty(totalPoints1); 
+        return  new SimpleIntegerProperty(totalPoints2); 
+    }
+    
+    public ReadOnlyObjectProperty<TeamId> winningTeamProperty(){
+        return new SimpleObjectProperty<TeamId>(winningTeam); 
+    }
+    
 
     public void setTurnPoints(TeamId team, int newTurnPoints) {
-        if (team == TeamId.TEAM_1)
+        if (team.equals(TeamId.TEAM_1))
             turnPoints1 = newTurnPoints;
         else
             turnPoints2 = newTurnPoints;
     }
+    public void setGamePoints(TeamId team, int newGamePoints) {
+        if (team.equals(TeamId.TEAM_1))
+            turnPoints1 = newGamePoints;
+        else
+            turnPoints2 = newGamePoints;
+    }
+    public void setTotalPoints(TeamId team, int newTotalPoints) {
+        if (team.equals(TeamId.TEAM_1))
+            turnPoints1 = newTotalPoints;
+        else
+            turnPoints2 = newTotalPoints;
+    }
+    public void setWinningTeam(TeamId team) {
+        winningTeam = team; 
+    }
+    
+    
     
     
 
