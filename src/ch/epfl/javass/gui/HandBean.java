e8d7dc363e1c934cb16a375f47b517686232284b
Alex <alexandre.santangelo@epfl.ch>
2019-04-28 18:48:49 +0200

    idk y but it doesn't work

--- src/ch/epfl/javass/gui/HandBean.java
+++ src/ch/epfl/javass/gui/HandBean.java
@@ -9,30 +9,56 @@
 package ch.epfl.javass.gui;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import ch.epfl.javass.jass.Card;
 import ch.epfl.javass.jass.CardSet;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.ObservableSet;
 
 public final class HandBean {
+
+    private final static int HAND_SIZE = 9;
+    private List<Card> hand = new ArrayList<>();
+    private Set<Card> playableCards = new HashSet<>();
     
-    private final static int HAND_SIZE = 9; 
-    private List<Card> hand = Arrays.asList(new Card[HAND_SIZE]); 
-    private List<Card> playableCards = Arrays.asList(new Card[HAND_SIZE]); 
-    
-    
+    public HandBean() {
+        for(int i=0; i<9; i++) {
+            hand.add(null); 
+            playableCards.add(null); 
+        }
+    }
+
     public void setHand(CardSet newHand) {
-        //TODO : CHECK :D
-        if(newHand.size()== 9)
-            for(int i=0; i<9; i++)
-                hand.add(newHand.get(i)); 
-        else
-            for(int i=0; i<9; i++) {
-                if(i>newHand.size())
-                    hand.add(null); 
-                else
-                    hand.add(newHand.get(i)); 
-            }        
+        // TODO : CHECK :D
+        for (int i = 0; i < HAND_SIZE; i++) {
+            if (i > newHand.size())
+                hand.add(null);
+            else
+                hand.add(newHand.get(i));
+        }
+        
+    }
+
+    public void setPlayableCards(CardSet newPlayableCards) {
+        for (int i = 0; i < HAND_SIZE; i++) {
+            if (i > newPlayableCards.size())
+                playableCards.add(null);
+            else
+                playableCards.add(newPlayableCards.get(i));
+        }
+    }
+
+    public ObservableList<Card> hand() {
+        return FXCollections.unmodifiableObservableList(
+                FXCollections.observableArrayList(hand));
+    }
+
+    public ObservableSet<Card> playableCards() {
+        return FXCollections.unmodifiableObservableSet(
+                FXCollections.observableSet(playableCards));
     }
 }
