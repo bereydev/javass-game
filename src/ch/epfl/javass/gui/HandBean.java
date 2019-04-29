175451f21011709597360081e90be2daee052a84
Alex <alexandre.santangelo@epfl.ch>
2019-04-29 14:07:17 +0200

    works!

--- src/ch/epfl/javass/gui/HandBean.java
+++ src/ch/epfl/javass/gui/HandBean.java
@@ -1,16 +1,11 @@
 /*
  *	Author : Alexandre Santangelo 
+
  *	Date   : Apr 27, 2019	
 */
 
-/**
- * 
- */
 package ch.epfl.javass.gui;
 
-
-import java.util.HashSet;
-import java.util.Set;
 
 import ch.epfl.javass.jass.Card;
 import ch.epfl.javass.jass.CardSet;
@@ -20,7 +15,6 @@ import javafx.collections.ObservableSet;
 
 public final class HandBean {
 
-    private final static int HAND_SIZE = 9;
     private ObservableList<Card> hand = FXCollections.observableArrayList(); 
     private ObservableSet<Card> playableCards = FXCollections.observableSet(); 
     
@@ -34,25 +28,25 @@ public final class HandBean {
     public void setHand(CardSet newHand) {
         if(newHand.size()==9)
             for(int i=0; i<9; i++) {
-                hand.remove(i); 
-                hand.add(i,newHand.get(i));
+                hand.set(i, newHand.get(i)); 
             }
         else
-            for(int i=0; i<10; i++) {
+            for(int i=0; i<9; i++) {
                 if(hand.get(i) == null)
                     continue; 
                 if(!newHand.contains(hand.get(i)))
-                    hand.remove(i); 
-                    hand.add(i,null); 
+                    hand.set(i, null); 
             }
         
     }
 
     public void setPlayableCards(CardSet newPlayableCards) {
-        if(newPlayableCards.size()==9)
+        if(newPlayableCards.size()==9) {
+            playableCards.clear();
             for(int i=0; i<9; i++) {
-                playableCards.add(newPlayableCards.get(i));
+                playableCards.add(newPlayableCards.get(i)); 
             }
+        }
         else
             for(Card c : playableCards) {
                 if(c == null)
