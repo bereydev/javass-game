fbc0723a6c5a7aea84fcbecfdf58c04d823dd3b4
Alex <alexandre.santangelo@epfl.ch>
2019-04-29 15:47:50 +0200

    Starting etape9

---
+++ src/ch/epfl/javass/gui/GraphicalPlayer.java
@@ -0,0 +1,22 @@
+/*
+ *	Author : Alexandre Santangelo 
+ *	Date   : Apr 29, 2019	
+*/
+
+/**
+ * 
+ */
+package ch.epfl.javass.gui;
+
+import java.util.Map;
+
+import ch.epfl.javass.jass.PlayerId;
+
+public class GraphicalPlayer {
+
+    public GraphicalPlayer(PlayerId player, Map<PlayerId, String> map,
+            HandBean hand, ScoreBean score) {
+        
+    }
+
+}
---
+++ src/ch/epfl/javass/gui/Testee.java
@@ -0,0 +1,41 @@
+/*
+ *	Author : Alexandre Santangelo 
+ *	Date   : Apr 28, 2019	
+*/
+
+/**
+ * 
+ */
+package ch.epfl.javass.gui;
+
+import ch.epfl.javass.jass.Card;
+import ch.epfl.javass.jass.Card.Color;
+import ch.epfl.javass.jass.Card.Rank;
+import ch.epfl.javass.jass.CardSet;
+import javafx.collections.ListChangeListener;
+
+public class Testee {
+
+    public static void main(String[] args) {
+        HandBean hb = new HandBean();
+        ListChangeListener<Card> listener = e -> System.out.println(e);
+        hb.hand().addListener(listener);
+
+        CardSet h = CardSet.EMPTY.add(Card.of(Color.SPADE, Rank.SIX))
+                .add(Card.of(Color.SPADE, Rank.NINE))
+                .add(Card.of(Color.SPADE, Rank.JACK))
+                .add(Card.of(Color.HEART, Rank.SEVEN))
+                .add(Card.of(Color.HEART, Rank.ACE))
+                .add(Card.of(Color.DIAMOND, Rank.KING))
+                .add(Card.of(Color.DIAMOND, Rank.ACE))
+                .add(Card.of(Color.CLUB, Rank.TEN))
+                .add(Card.of(Color.CLUB, Rank.QUEEN));
+        hb.setHand(h);
+        while (!h.isEmpty()) {
+            h = h.remove(h.get(0));
+            hb.setHand(h);
+        }
+
+    }
+
+}
