01d74f5013829d6f6d684dd7523479063f673625
Jonathan <jonathan.bereyziat@gmail.com>
2019-05-10 11:14:27 +0200

    Partie 11

---
+++ src/ch/epfl/javass/RemoteMain.java
@@ -0,0 +1,31 @@
+package ch.epfl.javass;
+
+import ch.epfl.javass.gui.GraphicalPlayerAdapter;
+import ch.epfl.javass.jass.Player;
+import ch.epfl.javass.net.RemotePlayerServer;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class RemoteMain extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Player player = new GraphicalPlayerAdapter();
+        Thread serverThread = new Thread(() -> {
+            RemotePlayerServer server = new RemotePlayerServer(player);
+            System.out.println("La partie commencera à la connexion du client...");
+            server.run();
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e) {
+            }
+        });
+        serverThread.setDaemon(true);
+        serverThread.start();
+
+    }
+}
