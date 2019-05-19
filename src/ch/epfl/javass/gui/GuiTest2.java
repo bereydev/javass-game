ff0bf659e7eac2e232e14a06aec99f04154e7f7f
Alex <alexandre.santangelo@epfl.ch>
2019-05-19 12:47:55 +0200

    ll

--- src/ch/epfl/javass/gui/GuiTest2.java
+++ src/ch/epfl/javass/gui/GuiTest2.java
@@ -18,9 +18,9 @@ public final class GuiTest2 extends Application {
   public void start(Stage primaryStage) throws Exception {
     Map<PlayerId, Player> ps = new EnumMap<>(PlayerId.class);
     ps.put(PlayerId.PLAYER_1, new GraphicalPlayerAdapter());
-    ps.put(PlayerId.PLAYER_2, new PacedPlayer(new MctsPlayer(PlayerId.PLAYER_2, 123, 10_000), 0.5));
-    ps.put(PlayerId.PLAYER_3, new PacedPlayer(new MctsPlayer(PlayerId.PLAYER_3, 456, 10_000), 0.5));
-    ps.put(PlayerId.PLAYER_4, new PacedPlayer(new MctsPlayer(PlayerId.PLAYER_4, 789, 10_000), 0.5));
+    ps.put(PlayerId.PLAYER_2, new PacedPlayer(new MctsPlayer(PlayerId.PLAYER_2, 123, 10_000), 1.5));
+    ps.put(PlayerId.PLAYER_3, new PacedPlayer(new MctsPlayer(PlayerId.PLAYER_3, 456, 10_000), 1.5));
+    ps.put(PlayerId.PLAYER_4, new PacedPlayer(new MctsPlayer(PlayerId.PLAYER_4, 789, 10_000), 1.5));
 
     Map<PlayerId, String> ns = new EnumMap<>(PlayerId.class);
     PlayerId.ALL.forEach(i -> ns.put(i, i.name()));
